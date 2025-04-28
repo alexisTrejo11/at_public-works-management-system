e909b4846ad6218624f0c8c43e8366ae8e8a8be5
Alexis Trejo <marcoalexispt.02@gmail.com>
2025-04-27 20:16:09 -0600

    initial commit: Set Spring Project

---
+++ src/main/java/com/publicworks/public_works_management/PublicWorksManagementApplication.java
@@ -0,0 +1,13 @@
+package com.publicworks.public_works_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicWorksManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicWorksManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/publicworks/public_works_management/PublicWorksManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicworks.public_works_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicWorksManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
